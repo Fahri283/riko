176c5e4e9b6c236778691a772b3368f8339ada67
fahri frayuda <frayudafahri@gmail.com>
2025-09-25 11:47:26 +0700

    setup awal

---
+++ tugas.php
@@ -0,0 +1,9 @@
+<?php
+// Daftar nama barang & harga dipisah
+$namaBarang = ["celak", "lipstik", "Parfum", "sampo", "Bedak"];
+$hargaBarang = [3000, 5000, 50000, 350000, 15000];
+
+echo "<h1>--POLGAN MART--</h1>";
+
+
+?>
